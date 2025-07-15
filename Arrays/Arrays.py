00db5a5224c73b1b7a768605b99a256681557f0b
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-07-15 23:12:21 +0600

    reverse an array

---
+++ Arrays/Arrays.py
@@ -0,0 +1,14 @@
+# This code defines a function to reverse an array in place.
+
+def reverseArr (arr):
+    left = 0
+    right = len(arr) -1
+
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
+    
+print(reverseArr([1,2,3,4,5]))
+
