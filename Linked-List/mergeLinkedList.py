d32729f4e63063a3491e7f629819c265dc8f96fd
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-08-04 22:24:37 +0600

    two sorted linked lists merge

---
+++ Linked-List/mergeLinkedList.py
@@ -0,0 +1,15 @@
+def mergeTwoLists(l1, l2):
+    dummy = Node(-1)
+    tail = dummy
+
+    while l1 and l2:
+        if l1.data < l2.data:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+
+    tail.next = l1 if l1 else l2
+    return dummy.next
